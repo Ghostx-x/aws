c21828b060187a6a4689d0723d3bd6cc8f2c7726
Ghostx-x <nataligevorgyan2007@gmail.com>
2025-09-10 20:14:49 +0400

    File upload with S3

--- handler.js
+++ handler.js
@@ -1,10 +1,30 @@
-export const helloWorld = async (event) => {
-    try {
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+const s3 = new S3Client({region: 'us-east-1'});
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const uploadFile = async (event) => {
+    try{
+        const body = JSON.parse(event.body);
+        const { fileName } = body;
+        const command = new PutObjectCommand({
+            Bucket: process.env.BUCKET_NAME,
+            Key: fileName,
+            ContentType: "application/octet-stream"
+        });
+
+        const uploadURL = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
         return {
             statusCode: 200,
-            body: JSON.stringify("Hello World!!!"),
+            body: JSON.stringify({ uploadURL }),
         };
-    } catch (error) {
-        console.error(error);
+    } catch(err) {
+        console.error("Error ", err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Internal server error" }),
+        };
     }
-}
+};
